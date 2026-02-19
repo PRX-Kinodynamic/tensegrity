559c221ac55aeef419fdbca7c34f7d19012dddf1
Will Johnson <will.johnson@yale.edu>
2026-02-19 18:29:07 -0500

    Edgar's fix for Vector

--- infraestructure/utils/include/tensegrity_utils/constants.hpp
+++ infraestructure/utils/include/tensegrity_utils/constants.hpp
@@ -1,22 +1,30 @@
 #pragma once
 
-namespace tensegrity
-{
-namespace constants
-{
-const double pi{ 3.1415926535897932385 };
-const double epsilon{ 1e-7 };
-constexpr double infinity{ std::numeric_limits<double>::infinity() };
+#include <Eigen/Core>
+#include <Eigen/Dense>
 
-int precision{ 9 };
-char separating_value{ ' ' };
+namespace tensegrity {
+namespace constants {
+const double pi{3.1415926535897932385};
+const double epsilon{1e-7};
+constexpr double infinity{std::numeric_limits<double>::infinity()};
 
-namespace color
-{
-constexpr std::string_view normal{ "\033[0m" };
-constexpr std::string_view red{ "\033[31m" };
-constexpr std::string_view green{ "\033[32m" };
-constexpr std::string_view yellow{ "\033[33m" };
-}  // namespace color
-}  // namespace constants
-}  // namespace tensegrity
+int precision{9};
+char separating_value{' '};
+
+namespace color {
+constexpr std::string_view normal{"\033[0m"};
+constexpr std::string_view red{"\033[31m"};
+constexpr std::string_view green{"\033[32m"};
+constexpr std::string_view yellow{"\033[33m"};
+} // namespace color
+} // namespace constants
+} // namespace tensegrity
+
+namespace Eigen {
+using Vector2d = Eigen::Matrix<double, 2, 1>;
+using Vector3d = Eigen::Matrix<double, 3, 1>;
+using Vector4d = Eigen::Matrix<double, 4, 1>;
+using Vector5d = Eigen::Matrix<double, 5, 1>;
+using Vector6d = Eigen::Matrix<double, 6, 1>;
+} // namespace Eigen
