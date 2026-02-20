a2a74daeaaedec9ca615ea002d1b711c3366a1e6
Will Johnson <will.johnson@yale.edu>
2026-02-19 20:46:52 -0500

    Revert "Gemini's take on Edgar's fix"
    This reverts commit 81390562e243d5f34df0f35903cd77907ff3acfc.

--- infraestructure/utils/include/tensegrity_utils/constants.hpp
+++ infraestructure/utils/include/tensegrity_utils/constants.hpp
@@ -2,8 +2,6 @@
 
 #include <Eigen/Core>
 #include <Eigen/Dense>
-#include <limits>        // Added for std::numeric_limits
-#include <string_view>   // Added for std::string_view
 
 namespace tensegrity {
 namespace constants {
@@ -11,9 +9,8 @@ const double pi{3.1415926535897932385};
 const double epsilon{1e-7};
 constexpr double infinity{std::numeric_limits<double>::infinity()};
 
-// Added 'inline' to prevent Multiple Definition linker errors
-inline int precision{9};
-inline char separating_value{' '};
+int precision{9};
+char separating_value{' '};
 
 namespace color {
 constexpr std::string_view normal{"\033[0m"};
@@ -24,8 +21,10 @@ constexpr std::string_view yellow{"\033[33m"};
 } // namespace constants
 } // namespace tensegrity
 
-// The actual patch for older Eigen 3.3 versions
 namespace Eigen {
-    template<typename T, int Size>
-    using Vector = Eigen::Matrix<T, Size, 1>;
+using Vector2d = Eigen::Matrix<double, 2, 1>;
+using Vector3d = Eigen::Matrix<double, 3, 1>;
+using Vector4d = Eigen::Matrix<double, 4, 1>;
+using Vector5d = Eigen::Matrix<double, 5, 1>;
+using Vector6d = Eigen::Matrix<double, 6, 1>;
 } // namespace Eigen
