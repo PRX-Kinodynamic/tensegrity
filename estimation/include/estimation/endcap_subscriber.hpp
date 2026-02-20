64e42ef9292e11ff949e6459b3056aebb5d904ea
Will Johnson <will.johnson@yale.edu>
2026-02-19 19:25:05 -0500

    PropagateNaN changed to .hasNaN

--- estimation/include/estimation/endcap_subscriber.hpp
+++ estimation/include/estimation/endcap_subscriber.hpp
@@ -32,7 +32,7 @@ struct rod_callback_t
     {
       auto zin = observations.endcaps[i];
       interface::copy(z, zin);
-      if (std::isnan(z.template maxCoeff<Eigen::PropagateNaN>()))
+      if (z.hasNaN())
       {
         observations.endcaps.erase(observations.endcaps.begin() + i);
       }
--- estimation/src/full_traj_estimation.cpp
+++ estimation/src/full_traj_estimation.cpp
@@ -452,7 +452,7 @@ void remove_invalid_observations(interface::TensegrityEndcaps& observations)
     auto zin = observations.endcaps[i];
     interface::copy(z, zin);
     // if (z.array().isNaN())
-    if (std::isnan(z.template maxCoeff<Eigen::PropagateNaN>()))
+    if (z.hasNaN())
     {
       observations.endcaps.erase(observations.endcaps.begin() + i);
       // nan_found = true;
