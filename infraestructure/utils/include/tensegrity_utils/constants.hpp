ff0d132cc14c53ac873e2d69f5b238cb5a89f435
Will Johnson <will.johnson@yale.edu>
2026-02-19 20:47:11 -0500

    Revert "Edgar's fix for Vector"
    This reverts commit 559c221ac55aeef419fdbca7c34f7d19012dddf1.

--- infraestructure/utils/include/tensegrity_utils/constants.hpp
+++ infraestructure/utils/include/tensegrity_utils/constants.hpp
@@ -1,30 +1,22 @@
 #pragma once
 
-#include <Eigen/Core>
-#include <Eigen/Dense>
+namespace tensegrity
+{
+namespace constants
+{
+const double pi{ 3.1415926535897932385 };
+const double epsilon{ 1e-7 };
+constexpr double infinity{ std::numeric_limits<double>::infinity() };
 
-namespace tensegrity {
-namespace constants {
-const double pi{3.1415926535897932385};
-const double epsilon{1e-7};
-constexpr double infinity{std::numeric_limits<double>::infinity()};
+int precision{ 9 };
+char separating_value{ ' ' };
 
-int precision{9};
-char separating_value{' '};
-
-namespace color {
-constexpr std::string_view normal{"\033[0m"};
-constexpr std::string_view red{"\033[31m"};
-constexpr std::string_view green{"\033[32m"};
-constexpr std::string_view yellow{"\033[33m"};
-} // namespace color
-} // namespace constants
-} // namespace tensegrity
-
-namespace Eigen {
-using Vector2d = Eigen::Matrix<double, 2, 1>;
-using Vector3d = Eigen::Matrix<double, 3, 1>;
-using Vector4d = Eigen::Matrix<double, 4, 1>;
-using Vector5d = Eigen::Matrix<double, 5, 1>;
-using Vector6d = Eigen::Matrix<double, 6, 1>;
-} // namespace Eigen
+namespace color
+{
+constexpr std::string_view normal{ "\033[0m" };
+constexpr std::string_view red{ "\033[31m" };
+constexpr std::string_view green{ "\033[32m" };
+constexpr std::string_view yellow{ "\033[33m" };
+}  // namespace color
+}  // namespace constants
+}  // namespace tensegrity
